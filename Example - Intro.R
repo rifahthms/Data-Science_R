b08da6b3cd42f1d5e6846654ee40c22c6aaaddb9
rifahthms <70227297+rifahthms@users.noreply.github.com>
2020-08-26 20:52:38 +0530

    Add files via upload

---
+++ Example - Intro.R
@@ -0,0 +1,25 @@
+1+3
+x<-100
+x+3
+sqrt (24)
+pi/2
+(6*3)+5/10
+y<-200
+2^2
+x<-c(1,1,2.5,4.5)
+y<-c(2,4,5,6)
+x+y
+z<-x+y
+x[2]
+x[2]<-4
+char<-c('rifahth','ch',7,1995)
+char
+df<-data.frame(a=1:3,y=c('rif','lol','nb'))
+
+df
+df1<-df[1,2]
+df1
+df2<-df[1,c(1,2)]
+df2
+df3<-df[1,c(1,2)]
+df3
